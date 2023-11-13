551e0175078aeb7bdb3d7eab4dd97cbe352120e0
EliasMateu <eliasmateusmgoncalves@gmail.com>
2023-11-13 15:28:25 -0300

    Conclusão do projeto

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
-class hero{
-    constructor(name, age, type){
+class hero {
+    constructor(name, age, type) {
         this.name = name
         this.age = age
         this.type = type
     }
-    atacar(){
-       let ataque = "";
+    atacar() {
+        //Criação de variaveis
+        let ataque = "";
+        let tipoAtaque = "";
+        let dano = ""
 
-       switch (this.type) {
-        case "mago":
-            ataque = "magia";
-            break;
-        case "guerreiro":
-            ataque = "espada";
-            break;
-        case "monge":
-            ataque = "artes marciais";
-            break;
-        case "ninja":
-            ataque = "shuriken";
-            break;
-        default:
-            console.log("Tipo de heroi nao reconhecido");
-            break;
-       }
-       console.log(`O ${this.type} atacou usando ${ataque}`)
+        //Verefica qual o tipo de heroi é e retorna os valores da variaveis
+        switch (this.type) {
+            case "mago":
+                ataque = "magia";
+                tipoAtaque = "bola de Fogo";
+                dano = 40;
+                break;
+            case "guerreiro":
+                ataque = "espada";
+                tipoAtaque = "Golpe giratorio";
+                dano = 35;
+                break;
+            case "monge":
+                ataque = "artes marciais";
+                tipoAtaque = "Chute com o pé";
+                dano = 25;
+                break;
+            case "ninja":
+                ataque = "shuriken";
+                tipoAtaque = "Shuriken explosiva";
+                dano = 30;
+                break;
+            default:
+                //Caso não escolha nenhum heroi dentro dos 4 estabelecidos
+                console.log("Tipo de heroi nao reconhecido");
+                break;
+        }
+        //Mensagem que ira ocorrer após a verificação do switch
+        console.log(`O ${this.type} atacou usando ${ataque} com o a tecnica ${tipoAtaque} e causou de dano ${dano}`);
     }
 }
-
+//Pequena historia para introduzir o usuario ao que é proposto
 console.log("Você controla um guilda com 4 participantes e encontra um desafio na floresta negra \nNisso voce se depara com uma missão na floresta negra");
 console.log("E ao adentrar logo se depara com uma embosca de globins e resolve atacar com a sua guilda, vamos ver oque cada um usou para se safar dessa");
+
 let heroi1 = new hero("Elias", 22, "mago");
 heroi1.atacar();
+
 let heroi2 = new hero("Kirito", 22, "guerreiro");
 heroi2.atacar();
+
 let heroi3 = new hero("Bruce", 22, "monge");
 heroi3.atacar();
+
 let heroi4 = new hero("Elizabeth", 22, "ninja");
 heroi1.atacar();
